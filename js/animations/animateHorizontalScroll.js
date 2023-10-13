722fc741a30a04179e626a57b89b8bb9dadf519a
jannispaul <jannis@pixel-pink.de>
2023-10-13 12:13:26 +0200

    added animate horizontal scroll

---
+++ js/animations/animateHorizontalScroll.js
@@ -0,0 +1,37 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export function animateHorizontalScroll(params) {
+  gsap.registerPlugin(ScrollTrigger);
+  const wrapper = document.querySelector("[animate='horizontal-scroll']");
+
+  // Scroll the section over
+  function scrollRightToMax(element) {
+    if (!element) return;
+    element.scroll({
+      // Scroll to maximum available value
+      left: element.scrollWidth - element.clientWidth,
+      behavior: "smooth",
+    });
+  }
+
+  // Scroll trigger to watch when section is in the right spot
+  gsap.from(wrapper, {
+    // x: "-20%",
+    ease: "power1.inOut",
+    duration: 1,
+    scrollTrigger: {
+      start: "top center",
+      end: "bottom 80%",
+      trigger: wrapper,
+      scrub: 1,
+      onUpdate: (trigger) => {
+        // console.log(trigger, wrapper.scrollWidth, wrapper.clientWidth);
+
+        if (trigger.progress > 0.5) {
+          scrollRightToMax(wrapper);
+        }
+      },
+    },
+  });
+}
--- js/index.js
+++ js/index.js
@@ -2,26 +2,35 @@
 import { moveImgInText } from "./utils/moveImgInText.js";
 import { onAllPages } from "./utils/onAllPages.js";
 import { animateHalfs } from "./animations/animateHalfs.js";
-import { animateFooter } from "./animations/animateFooter.js";
+// import { animateFooter } from "./animations/animateFooter.js";
 import { animateScaleUp } from "./animations/animateScaleUp.js";
 import { animateImageScale } from "./animations/animateImageScale.js";
 import { animateQuotes } from "./animations/animateQuotes.js";
 import { animateHow } from "./animations/animateHow.js";
+
 import { headerVideoProgress } from "./utils/headerVideoProgress.js";
+import { getScreenSize } from "./utils/getScreenSize.js";
+import { animateHorizontalScroll } from "./animations/animateHorizontalScroll.js";
 
 function init() {
   onAllPages();
   moveImgInText();
   headerVideoProgress();
+  animateScaleUp();
+  animateImageScale();
+  // animateFooter();
+
+  let isMobile = getScreenSize();
+  // Only run on mobile
+  if (isMobile) {
+    animateHorizontalScroll();
+  }
   // Only run on desktop
-  if (window.matchMedia("(min-width: 768px)").matches) {
+  if (!isMobile) {
     animateHalfs();
     animateQuotes();
     animateHow();
   }
-  animateScaleUp();
-  animateImageScale();
-  animateFooter();
 }
 
 if (document.readyState == "loading") {
