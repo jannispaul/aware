08ad677fac9bf0a48268fbf7bd5e691d6d930d91
jannispaul <jannis@pixel-pink.de>
2023-10-16 10:17:00 +0200

    added lottie support to halfs section

--- js/animations/animateHalfs.js
+++ js/animations/animateHalfs.js
@@ -8,14 +8,14 @@ export function animateHalfs() {
 
   let leftHalf = document.querySelector("[animate='left-half']");
   let rightHalf = document.querySelector("[animate='right-half']");
-  let videoLeft = leftHalf?.querySelector("video");
+  // let videoLeft = leftHalf?.querySelector("video");
+  let lottieLeft = leftHalf?.querySelector("lottie-player");
   let videoRight = rightHalf?.querySelector("video");
 
   if (!leftHalf || !rightHalf) return;
 
   // Animations
   let growLeft = gsap.timeline({ paused: true }).to(rightHalf.querySelector("h3"), { opacity: 0, duration: 0.4, display: "none" }).to(leftHalf, { duration: 0.7, flex: "1.5 1 0%", ease: "power2.inOut" }, 0);
-
   let growRight = gsap.timeline({ paused: true }).to(leftHalf.querySelector("h3"), { opacity: 0, duration: 0.4, display: "none" }).to(rightHalf, { duration: 0.7, flex: "1.5 1 0%", ease: "power2.inOut" }, 0);
 
   leftHalf.addEventListener("mouseenter", () => {
@@ -29,10 +29,14 @@ export function animateHalfs() {
 
   rightHalf.addEventListener("mouseenter", () => {
     growRight.play();
-    videoLeft?.pause();
+    if (!lottieLeft) return;
+    // @ts-ignore
+    lottieLeft?.pause();
   });
   rightHalf.addEventListener("mouseleave", () => {
     growRight.reverse();
-    videoLeft?.play();
+    if (!lottieLeft) return;
+    // @ts-ignore
+    lottieLeft?.play();
   });
 }
