e368c17fb76121343fad880c6c5d2411c6ec7fad
jannispaul <jannis@pixel-pink.de>
2023-10-18 08:27:10 +0200

    added intersection observer

--- js/animations/animateQuotesMobile.js
+++ js/animations/animateQuotesMobile.js
@@ -26,5 +26,43 @@ export function animateQuotesMobile(params) {
   // Add center class on load once
   updateCenterItemClass();
   // Update classes when index changes
-  slider?.events.on("indexChanged", updateCenterItemClass);
+  // slider?.events.on("indexChanged", updateCenterItemClass);
+
+  // Intersection observer to watch
+  // Get an array of elements to observe
+  const elementsToObserve = document.querySelectorAll("[data-element='quote']");
+
+  // Observer options
+  const options = {
+    // root: null,<-- root can not be null for rootMargin to work
+    rootMargin: "100% 0% 100% 0%", // Only fire on horizontal since the vertical margins are set 100% outside the viewport
+    threshold: [0, 0.9], // Trigger when the entire element enters (0) or exits (1) the viewport vertically
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const intersectionRatio = entry.intersectionRatio;
+        // Check if the element is fully visible horizontally
+        if (intersectionRatio >= 0.9) {
+          // Add class when element enters from either left or right
+          entry.target.classList.add("center");
+        } else {
+          // Remove class when element exits to the side
+          entry.target.classList.remove("center");
+        }
+      } else {
+        // Remove class when element exits to the side
+        entry.target.classList.remove("center");
+      }
+    });
+  };
+
+  // Create Intersection Observer instance
+  const observer = new IntersectionObserver(observerCallback, options);
+
+  // Observe each element in the array
+  elementsToObserve.forEach((element) => {
+    observer.observe(element);
+  });
 }
